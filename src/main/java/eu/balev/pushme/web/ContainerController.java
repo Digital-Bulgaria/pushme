ec76f4ca8e430b5107825ed6a86fd1a34130e3dc
Lachezar Balev <l_@mail.bg>
2016-12-28 21:47:22 +0200

    Introducing a navbar

--- src/main/java/eu/balev/pushme/web/ContainerController.java
+++ src/main/java/eu/balev/pushme/web/ContainerController.java
@@ -3,6 +3,7 @@ package eu.balev.pushme.web;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -51,13 +52,20 @@ public class ContainerController {
 	public ModelAndView inspectContainer(
 			@RequestParam(value = "id", required = false) String id) {
 
+		ModelAndView result = new ModelAndView();
+		
 		Container container = containerRepo.findOne(id);
 		
-		ModelAndView result = new ModelAndView();
-
-		result.setViewName("inspectcontainer");
-		result.addObject("container", container);
-
+		if (container == null)
+		{
+			result.setStatus(HttpStatus.NOT_FOUND);
+			result.setViewName("containernotfound");
+		}
+		else
+		{
+			result.setViewName("inspectcontainer");
+			result.addObject("container", container);
+		}
 		return result;
 	}
 }
