5a6b2ba442713105e983125f8be111da9ad263c1
Lachezar Balev <l_@mail.bg>
2016-12-28 11:20:45 +0200

    Dynamically load headers

--- src/main/static/js/inspectcontainer.js
+++ src/main/static/js/inspectcontainer.js
@@ -1,15 +1,28 @@
-function loadRequest(containerid, requestid)
+function loadRequest(dataCtnr)
 {
+	var containerid = $( dataCtnr ).data( "ctnr-id" );
+	var requestid = $( dataCtnr ).data( "request-id" );
+	
 	$( "#req-id-" + requestid ).
 		load( "/requests-details?containerid=" + containerid + "&requestid=" + requestid);
 }
 
 $(function() {
 	
+	//preload the latest request
 	$( ".req-container-preload" ).each(function() {
-		var containerid = $( this ).data( "ctnr-id" );
-		var requestid = $( this ).data( "request-id" );
+		loadRequest(this);
+	});
+	
+	//load a specific request
+	$( ".req-container > a ").click(function(event) {
 		
-		loadRequest(containerid, requestid);
+		event.preventDefault();
+		var dataCtnr = $(this).parent();
+		
+		$(this).hide();
+		$(dataCtnr).children('.req-container-spinner').show();
+		
+		loadRequest(dataCtnr);
 	});
 })
