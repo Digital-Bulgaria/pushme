77bb33485979850e4b62e26132b7c293d48fadb6
Lachezar Balev <l_@mail.bg>
2017-01-08 11:25:56 +0200

    Working on the login page

--- src/main/java/eu/balev/pushme/web/LoginController.java
+++ src/main/java/eu/balev/pushme/web/LoginController.java
@@ -1,14 +1,28 @@
 package eu.balev.pushme.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class LoginController {
 
-	@RequestMapping("/login")
-    public String home() {
-        return "login";
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoginController.class);
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView getLoginPage(@RequestParam(name="error", required=false)  String error) {
+        LOGGER.debug("Serving a login page, error={}", error);
+        
+        ModelAndView ret = new ModelAndView();
+        
+        ret.setViewName("login");
+        ret.addObject("error", error);
+        
+        return ret;
     }
-	
+
 }
