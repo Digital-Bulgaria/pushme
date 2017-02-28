1c4f203b940b40befe9bd3adc2dd3f6b09c2d6a7
Lachezar Balev <l_@mail.bg>
2017-02-28 17:44:37 +0200

    Getting ready for container update

--- src/main/java/eu/balev/pushme/web/container/ContainerController.java
+++ src/main/java/eu/balev/pushme/web/container/ContainerController.java
@@ -2,15 +2,19 @@ package eu.balev.pushme.web.container;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import eu.balev.pushme.domain.Container;
 import eu.balev.pushme.domain.Request;
@@ -22,71 +26,92 @@ import eu.balev.pushme.service.request.RequestService;
 @Controller
 public class ContainerController {
 
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ContainerController.class);
+
 	@Autowired
 	private ContainerService containerService;
-	
-	@Autowired 
+
+	@Autowired
 	private RequestService requestService;
-	
+
 	@Autowired
 	private ContainerRepository containerRepo;
 
 	@RequestMapping(value = "/container/{id}")
-	public ResponseEntity<String> pushInContainer(@PathVariable("id") String id,
-			HttpServletRequest httpRequest) {
-		
-		//get the container
+	public ResponseEntity<String> pushInContainer(
+			@PathVariable("id") String id, HttpServletRequest httpRequest) {
+
+		// get the container
 		Container container = containerRepo.findOne(id);
-		
+
 		Request pushMeReq = requestService.mapRequest(httpRequest);
-		
+
 		containerService.storeRequest(container, pushMeReq);
-		
+
 		Rule rule = container.getBestRule(pushMeReq);
-		
+
 		ResponseEntity<String> response = new ResponseEntity<>(
-				rule.getResponseBody(), 
-				HttpStatus.valueOf(rule.getResponseCode()));
-		
+				rule.getResponseBody(), HttpStatus.valueOf(rule
+						.getResponseCode()));
+
 		return response;
 	}
 
 	@RequestMapping(value = "/container-inspect", method = RequestMethod.GET)
-	public ModelAndView inspectContainer(
-			@RequestParam(value = "id") String id) {
+	public ModelAndView inspectContainer(@RequestParam(value = "id") String id) {
 
 		return processContainer("inspectcontainer", id);
 	}
-	
+
 	@RequestMapping(value = "/container-setup", method = RequestMethod.GET)
-	public ModelAndView setupContainer(
-			@RequestParam(value = "id") String id,
-			@RequestParam(value = "newrule", required=false) String newrule) {
+	public ModelAndView setupContainer(@RequestParam(value = "id") String id,
+			@RequestParam(value = "newrule", required = false) String newrule) {
 
 		ModelAndView ret = processContainer("setupcontainer", id);
-		
-		if (newrule != null)
-		{
+
+		if (newrule != null) {
 			ret.addObject("newrule", newrule);
 		}
-		
+
 		return ret;
 	}
-	
-	private ModelAndView processContainer(String viewName, 
-			String ctnrID)
-	{
+
+	@PostMapping(value = "/container-rename")
+	public String renameContainer(
+			@RequestParam("containerId") String containerId,
+			@RequestParam(name="newName", required=false) String newName,
+			RedirectAttributes redirectAttributes) {
+
+		LOGGER.debug("Received rename request for container with id {}.",
+				containerId);
+
+		Container ctnr = containerRepo.findOne(containerId);
+		if (ctnr == null) {
+			LOGGER.debug("Unable to find container by id {}.", containerId);
+		} else if (newName == null || newName.isEmpty()) {
+			LOGGER.debug(
+					"Rename for container with id {} was requested but no new name was provided.",
+					containerId);
+		} else {
+			LOGGER.debug("Renaming container with id {}.", containerId);
+			ctnr.setName(newName);
+			containerRepo.save(ctnr);
+
+			redirectAttributes.addAttribute("id", containerId);
+		}
+		return "redirect:/container-setup";
+	}
+
+	private ModelAndView processContainer(String viewName, String ctnrID) {
 		ModelAndView result = new ModelAndView();
-		
+
 		Container container = containerRepo.findOne(ctnrID);
-		
-		if (container == null)
-		{
+
+		if (container == null) {
 			result.setStatus(HttpStatus.NOT_FOUND);
 			result.setViewName("containernotfound");
-		}
-		else
-		{
+		} else {
 			result.setViewName(viewName);
 			result.addObject("container", container);
 		}
